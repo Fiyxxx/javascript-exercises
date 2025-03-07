1cc7e46e88ffb90f31ed44ac9658fe8080f75372
Hans <gohhansheng@outlook.com>
2025-03-07 17:33:30 +0800

    Pass 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let returnstr = "";
+    for (let i = 0; i < num; i++) {
+        returnstr += `${string}`;
+    }
+    return returnstr;
 };
 
 // Do not edit below this line
