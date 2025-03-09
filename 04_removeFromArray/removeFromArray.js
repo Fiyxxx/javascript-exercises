5a4d9194080e52079f50a1e827284c3db1521a8d
Hans <gohhansheng@outlook.com>
2025-03-09 19:13:40 +0800

    Pass 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
 const removeFromArray = function() {
+    let array = arguments[0];
+
+    for (let i = 1; i < arguments.length; i++) {
+        for (let counter = 0; counter < array.length; counter++) {
+            if (array[counter] === arguments[i]) {
+                array.splice(counter,1);
+                counter--;
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
