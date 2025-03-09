727d1e7e04ad1167296bf915281c76848e4c90d4
Hans <gohhansheng@outlook.com>
2025-03-09 23:24:56 +0800

    Pass 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    let temp;
+    let sum = 0;
 
+    if (typeof first != "number" || typeof last != "number"
+        || (first + last) % 1 != 0 || first < 0 || last < 0
+    ) {
+        return "ERROR";
+    }
+
+    if (first > last) {
+        temp = first;
+        first = last;
+        last = temp;
+    }
+
+    for (let add = first; add <= last; add++) {
+        sum += add;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
